7036e99ce21e792cffc51bdc0539bd6bd11f6096
Bryan <birlbeck@comcast.net>
2022-03-02 14:46:59 -0500

    adding diy network app to appropriate branch

--- src/Shared.java
+++
@@ -1,9 +0,0 @@
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-public class Shared {
-	static double count;
-	static BlockingQueue<DataSlice> sliceQueue = new LinkedBlockingQueue<DataSlice>(1000);
-	static double sum;
-	static int workerCount = 0;
-}
